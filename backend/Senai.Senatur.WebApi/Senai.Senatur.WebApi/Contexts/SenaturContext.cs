c405ad673e7bf07996759ca3570847bc06a3ca17
FIC <FIC@info.local>
2020-03-11 16:58:41 -0300

    Postman

---
+++ backend/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Contexts/SenaturContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Senai.Senatur.WebApi.Domains
+{
+    public partial class SenaturContext : DbContext
+    {
+        public SenaturContext()
+        {
+        }
+
+        public SenaturContext(DbContextOptions<SenaturContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Pacotes> Pacotes { get; set; }
+        public virtual DbSet<TiposUsuario> TiposUsuario { get; set; }
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DEV701\\SQLEXPRESS; Initial Catalog=Senatur_Tarde; user Id=sa; pwd=sa@132;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pacotes>(entity =>
+            {
+                entity.HasKey(e => e.IdPacote);
+
+                entity.Property(e => e.Ativo).HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.DataIda).HasColumnType("date");
+
+                entity.Property(e => e.DataVolta).HasColumnType("date");
+
+                entity.Property(e => e.Descricao)
+                    .IsRequired()
+                    .HasColumnType("text");
+
+                entity.Property(e => e.NomeCidade)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NomePacote)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TiposUsuario>(entity =>
+            {
+                entity.HasKey(e => e.IdTiposUsuario);
+
+                entity.Property(e => e.Titulo)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdTiposUsuarioNavigation)
+                    .WithMany(p => p.Usuarios)
+                    .HasForeignKey(d => d.IdTiposUsuario)
+                    .HasConstraintName("FK__Usuarios__IdTipo__5535A963");
+            });
+        }
+    }
+}
---
+++ backend/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Controllers/PacoteController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Senatur.WebApi.Domains;
+using Senai.Senatur.WebApi.Interfaces;
+using Senai.Senatur.WebApi.Repositories;
+
+namespace Senai.Senatur.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacoteController : ControllerBase
+    {
+        private IPacote _pacoteRepository;
+
+        public PacoteController()
+        {
+            _pacoteRepository = new PacoteRepository();
+        }
+
+        /// <summary>
+        /// Controller responsável por listar os pacotes de viagens da Senatur
+        /// </summary>
+        /// <response code="200">retorna um ok e uma listar</response>
+        [Authorize]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            return Ok(_pacoteRepository.Listar());
+        }
+
+        /// <summary>
+        /// Controller responsável pelos buscar pelo Id Mandar um Id Retorna o pacote daquele Id do Senatur
+        /// </summary>
+        /// <response code="200">retorna um usuario do Id Solicitando</response>
+        /// <response code="404">caso Id não existe retorna um Not Found</response> 
+        [Authorize(Roles = "1")]
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(int id)
+        {
+            var buscar = _pacoteRepository.BuscarPorId(id);
+            if (buscar == null)
+            {
+                return NotFound("Id não existe");
+            }
+            else
+            {
+                return Ok(buscar);
+            }
+        }
+
+        /// <summary>
+        /// Controller responsável pelos Cadastrar um novo pacote de viagens da Senatur
+        /// </summary>
+        /// <response code="201">retorna um Created caso for criado</response>
+        /// <response code="404">caso estiver algun campo nulo retorna um not found</response> 
+        [Authorize(Roles = "1")]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(Pacotes pacotes)
+        {
+            if(pacotes == null)
+            {
+                return NotFound("algun campo não preenchido");
+            }else
+            {
+            _pacoteRepository.AdicionarPacote(pacotes);
+            return StatusCode(201);
+            }
+        }
+
+        /// <summary>
+        /// Controller responsável pelos Atualizar um pacote existente na Senatur
+        /// </summary>
+        /// <response code="202">retorna um aceito e atualizar um pacote</response>
+        [Authorize(Roles = "1")]
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        public IActionResult atualizar(Pacotes pacotes)
+        {
+            _pacoteRepository.AtualizarIdCorpo(pacotes);
+            return StatusCode(202, "seu TipoUsuario foi atualizado com sucesso");
+        }
+
+        /// <summary>
+        /// Controller responsável pelos Deletar um pacote da Senatur
+        /// </summary>
+        /// <response code="202">retorna um aceito e deletar seu pacote</response>
+        [Authorize(Roles = "1")]
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        public IActionResult deletar(int id)
+        {
+            _pacoteRepository.Deletar(id);
+            return StatusCode(202, "seu TipoUsuario foi deletado com sucesso");
+        }
+    }
+}
